9c222baa9ede93272bf39bde7506fc5fa14508d7
SchizoBoy <cadonienzo@gmail.com>
2022-10-10 15:49:25 +0200

    build : init projet gradle

---
+++ projet/src/main/java/projet/Main.java
@@ -0,0 +1,10 @@
+package projet;
+
+/** Classe principale du projet */
+
+public class Main{
+  /** Fonction principale du programme */
+  public static void main(String[] args) {
+    System.out.println("Begin");
+  }
+}
