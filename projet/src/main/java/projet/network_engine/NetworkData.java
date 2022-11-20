bc715c9a059101908c0cf6b89da1c89fb82678d8
Jean Chaput <jean.chaput@protonmail.com>
2022-11-20 14:21:09 +0100

    feat : classe NetworkData devenue abstraite pour pouvoir être définie par l'utilisateur

--- projet/src/main/java/projet/network_engine/NetworkData.java
+++ projet/src/main/java/projet/network_engine/NetworkData.java
@@ -9,36 +9,6 @@ import java.io.Serializable;    // To allow network transmission
 /***
  * Data exchanged between Client and Server
  */
-public class NetworkData implements Serializable {
-
-    /***** PARAMETERS *****/
-
-    String message;
-
-    /***** METHOD *****/
-
-    /***
-     * Constructor
-     * @param _message
-     */
-    public NetworkData(String _message) {
-        message = _message;
-    }
-
-    /***
-     * Transform a NetworkData object into a String
-     * @return The String
-     */
-    @Override
-    public String toString(){
-        return message;
-    }
-
-    /***
-     * Transform a string into a NetworkData object
-     * @return The NetworkData object
-     */
-    public NetworkData toNetworkData(String message) {
-        return new NetworkData(message);
-    }
+public abstract class NetworkData implements Serializable {
+    // Data defined by the user to suit its needs
 }
