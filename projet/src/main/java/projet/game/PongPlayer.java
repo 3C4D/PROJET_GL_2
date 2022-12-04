7103898e5161d1c316209a10f7cefd006aa666c7
Jean Chaput <jean.chaput@protonmail.com>
2022-12-05 00:02:31 +0100

    feat : ajout des classes réseau pour le pong

---
+++ projet/src/main/java/projet/game/PongPlayer.java
@@ -0,0 +1,15 @@
+package projet.game;
+
+/***** IMPORTS *****/
+
+// Client
+import projet.network_engine.Client;
+
+/***** CLASS *****/
+
+/**
+ * PongPlayer class is a simple client to exchange information with a PongServer
+ */
+public class PongPlayer extends Client {
+    // Nothing here for now
+}
---
+++ projet/src/main/java/projet/game/PongServer.java
@@ -0,0 +1,50 @@
+package projet.game;
+
+/***** IMPORTS *****/
+
+// Server
+import projet.network_engine.Server;
+
+/***** CLASS *****/
+
+/**
+ * PongServer class that extends the server class from the network engine to
+ * define the server routine and a new constructor
+ */
+public class PongServer extends Server {
+
+    /***** PARAMETERS *****/
+
+    MyWorldSPP world;
+
+    /***** CONSTRUCTORS *****/
+
+    /**
+     * Constructor for the PongServer class
+     * 
+     * @param _port
+     * @param _clientsNumber
+     * @param _world
+     */
+    public PongServer(int _port, int _clientsNumber, MyWorldSPP _world) {
+        super(_port, _clientsNumber);
+        world = _world;
+    }
+
+    /***** METHODS *****/
+
+    /**
+     * Definition of the server routine
+     * @param username The username of the client
+     */
+    @Override
+    public void runningRoutine(String username) {
+        String message;
+        while (true) {
+            if (messages.size() > 0) {
+                message = messages.remove().toString();
+                diffuseMessage(message, message.split(" ")[1]);
+            }
+        }
+    }
+}
